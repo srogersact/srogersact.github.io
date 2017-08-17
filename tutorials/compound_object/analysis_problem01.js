8f12d1e7e15c38d7768d46f19fca3aaea7943a56
srogersact <srogersact@gmail.com>
2017-08-17 11:13:41 -0700

    Made folders. Got js and problem html set up in each.

---
+++ tutorials/compound_object/analysis_problem01.js
@@ -0,0 +1,71 @@
+// COMPOUND OBJECT
+
+// This has been modified by setting var i to 1.
+
+
+var $ = function(id) {
+    return document.getElementById(id);
+};
+
+var problem = [
+{
+analyzedSentence: 'Blank. Blank.',
+composedSentence: 'Blank and blank.',
+},
+{
+analyzedSentence: 'Egyptians valued order. Egyptians valued stability.',
+composedSentence: 'Egyptians valued order and stability.',
+},
+{
+analyzedSentence: 'The pharaoh wore the short, red crown of Lower Egypt. The pharaoh also wore the tall, white crown of Upper Egypt.',
+composedSentence: 'The pharaoh wore the short, red crown of Lower Egypt and the tall, white crown of Upper Egypt.',
+},
+{
+analyzedSentence: 'Tutankhamun had a cobra on the left side of his crown. Tutankhamun also had a vulture on the right side of his crown.',
+composedSentence: 'Tutankhamun had a cobra on the left side of his crown and a vulture on the right side of his crown.',
+},
+{
+analyzedSentence: 'The pharaohs commanded the Egyptians to build irrigation canals. The pharaohs also commanded the Egyptians to build monuments.',
+composedSentence: 'The pharaohs commanded the Egyptians to build irrigation canals and monuments.',
+},
+{
+analyzedSentence: 'The tomb Ramses II built for his sons has three levels. The tomb Ramses II built for his sons also has hundreds of small rooms.',
+composedSentence: 'The tomb Ramses II built for his sons has three levels and hundreds of small rooms.',
+},
+{
+analyzedSentence: 'Later in his life Ramses II made peace. Later in his life Ramses II did not make war.',
+composedSentence: 'Later in his life Ramses II made peace instead of war.',
+},
+{
+analyzedSentence: 'Changes in religion were unwelcome in Egypt. Changes in art were also unwelcome in Egypt.',
+composedSentence: 'Changes in religion and art were unwelcome in Egypt.',
+},
+
+];
+
+	var i = 1
+   document.write(problem[i].composedSentence);
+
+var approveSolution = function() {
+	var sentenceSolution = $("sentence_solution").value;
+   var isValid = true;
+	
+	if (sentenceSolution === "") { 
+		alert("Please give it a try.");
+		isValid = false;
+	} else
+
+	if (sentenceSolution !== problem[i].analyzedSentence) { 
+		alert("Oops! Please try again!");
+		isValid = false;
+		} if (isValid) { 
+		alert("Way to go! Now you have sentences that are easier to understand!!"); 
+			$("sentence_solution_form").submit(); 
+		}
+};
+
+ window.onload = function() {
+	$("sentence_solution_approval").onclick = approveSolution;
+    $("sentence_solution").focus();
+//    i++;
+};
---
+++ tutorials/past_participle/analysis_problem01.js
@@ -0,0 +1,67 @@
+// PAST PARTICIPLE
+
+// This has been modified by setting var i to 1.
+
+
+var $ = function(id) {
+    return document.getElementById(id);
+};
+
+var problem = [
+{
+analyzedSentence: 'Blank. Blank.',
+composedSentence: 'Blank and blank.',
+},
+{
+analyzedSentence: 'The Olympian gods were worshiped by all Greeks. The Olympian gods lived on Mt. Olympus.',
+composedSentence: 'Worshiped by all Greeks, the Olympian gods lived on Mt. Olympus.',
+},
+{
+analyzedSentence: 'A building fund was taken from the general fund. The building fund would pay for the workers and materials.',
+composedSentence: 'Taken from the general fund, a building fund would pay for the workers and materials.',
+},
+{
+analyzedSentence: 'The construction of the temple was supervised by the architect. The construction of the temple lasted for years.',
+composedSentence: 'Supervised by the architect, the construction of the temple lasted for years.',
+},
+{
+analyzedSentence: 'The simple Doric style of temple was preferred in mainland Greece. The simple Doric style of temple was developed a few decades before the more elaborate Ionic style.',
+composedSentence: 'Preferred in mainland Greece, the simple Doric style of temple was developed a few decades before the more elaborate Ionic style.',
+},
+{
+analyzedSentence: 'Construction cranes are often used by modern builders. Construction cranes are essential for lifting steel beams to great heights.',
+composedSentence: 'Often used by modern builders, construction cranes are essential for lifting steel beams to great heights.',
+},
+{
+analyzedSentence: 'The Greek style of temple was developed during the Classical period. The Greek style of temple has been an inspiration to architects ever since.',
+composedSentence: 'Developed during the Classical period, the Greek style of temple has been an inspiration to architects ever since.',
+},
+
+];
+
+	var i = 1
+   document.write(problem[i].composedSentence);
+
+var approveSolution = function() {
+	var sentenceSolution = $("sentence_solution").value;
+   var isValid = true;
+	
+	if (sentenceSolution === "") { 
+		alert("Please give it a try.");
+		isValid = false;
+	} else
+
+	if (sentenceSolution !== problem[i].analyzedSentence) { 
+		alert("Oops! Please try again!");
+		isValid = false;
+		} if (isValid) { 
+		alert("Way to go! Now you have sentences that are easier to understand!!"); 
+			$("sentence_solution_form").submit(); 
+		}
+};
+
+ window.onload = function() {
+	$("sentence_solution_approval").onclick = approveSolution;
+    $("sentence_solution").focus();
+//    i++;
+};
