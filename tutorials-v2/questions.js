fc101995fb421a9175ce005b47f768af912df156
srogersact <srogersact@gmail.com>
2017-12-23 15:45:50 -0500

    Small changes.

--- tutorials-v2/questions.js
+++ tutorials-v2/questions.js
@@ -1,47 +1,47 @@
 var questions = [
-    {
-        "statement": "How hard was that?",
-        "answer": "real hard"
-    },
-    {
-        "statement": "How hard was WHAT?",
-        "answer": "IDK..."
-    },
-    {
-        "statement": "2+2?",
-        "answer": "2+2 = 4"
-    },
-    {
-    "statement": "2+2?",
-    "answer": "2+2 = 4"
-    },
-    {
-    "statement": "In which year did Christopher Columbus discover America?",
-    "answer": "1492"
-    },
-    {
-    "statement": "What is the answer to life, the universe, and everything?",
-    "answer": "42"
-    },
-    {
-    "statement": "What is the answer to life, the universe, and everything?",
-    "answer": "42"
-    },
-    {
-    "statement": "What is the answer to life, the universe, and everything?",
-    "answer": "42"
-    },
-    {
-    "statement": "What is the answer to life, the universe, and everything?",
-    "answer": "42"
-    },
-    {
-    "statement": "What is the answer to life, the universe, and everything?",
-    "answer": "42"
-    },
-    {
-    "statement":
-      "What occurs twice in a lifetime, but once in every year, twice in a week but never in a day?",
-    "answer": "The E letter"
-    }
+{
+"answer": 'Here is another answer.',
+"statement": 'Here is another question.',
+},
+{
+"answer": 'Smelters added small amounts of tin to copper. Smelters discovered how to make bronze.',
+"statement": 'Adding small amounts of tin to copper, smelters discovered how to make bronze.',
+},
+{
+"answer": 'The smelting process produces an impure iron mixture at lower temperatures. The smelting process had to be followed by vigorous hammering.',
+"statement": 'Producing an impure iron mixture at lower temperatures, the smelting process had to be followed by vigorous hammering.',
+},
+{
+"answer": 'The Bronze Age civilization at Mycenae collapsed around 1200 B.C.E. The Bronze Age civilization at Mycenae was forgotten, but its giant buildings remained.',
+"statement": 'Collapsing around 1200 B.C.E., the Bronze Age civilization at Mycenae was forgotten, but its giant buildings remained.',
+},
+{
+"answer": 'The Greeks usually preferred to use limestone for for less important buildings. The Greeks built their most important buildings with marble.',
+"statement": 'Usually preferring to use limestone for for less important buildings, the Greeks built their most important buildings with marble.',
+},
+{
+"answer": 'Chersiphron turned each column drum on its side. Chersiphron attached a pair of axles and a wooden frame so he could hitch a team of oxen to the drum and roll it to the building site.',
+"statement": 'Turning each column drum on its side, Chersiphron attached a pair of axles and a wooden frame so he could hitch a team of oxen to the drum and roll it to the building site.',
+},
+{
+"answer": 'Greek builders used the same method the Egyptians had used for centuries. Greek builders built temporary earthen ramps and used wooden rollers to move heavy stone blocks into higher positions.',
+"statement": 'Using the same method the Egyptians had used for centuries, Greek builders built temporary earthen ramps and used wooden rollers to move heavy stone blocks into higher positions.',
+},
+{
+"answer": 'They used ropes to lift the blocks. They did not leave rope underneath the blocks.',
+"statement": 'They used ropes to lift the blocks without leaving rope underneath the blocks.',
+},
+{
+"answer": 'They carved U-shaped grooves into the end of a block. This allowed the block to be lifted without passing a rope underneath.',
+"statement": 'Carving U-shaped grooves into the end of a block allowed the block to be lifted without passing a rope underneath.',
+},
+{
+"answer": 'They designed the columns so that they bulge at the center. This is called entasis.',
+"statement": 'Designing the columns so that they bulge at the center is called entasis.',
+},
+{
+"answer": 'They put iron dowels in the exact centers of the tops and bottoms of the drums. This allowed the masons to align the drums accurately.',
+"statement": 'Putting iron dowels in the exact centers of the tops and bottoms of the drums allowed the masons to align the drums accurately.',
+}
+
 ];
