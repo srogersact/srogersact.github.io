9b19a73ca21050ab2c8fa8eac193b96166a9e15b
Travis Peters <traviswp@gmail.com>
2017-12-24 18:31:10 -0500

    text area styleeeee

--- tutorials-v2/compound_sentences/script.js
+++ tutorials-v2/compound_sentences/script.js
@@ -55,6 +55,7 @@ window.addEventListener("load", e => {
                 parentNode.insertBefore(feedbackElement, targetNode.nextSibling);
             }
             feedbackElement.id = feedbackElementId;
+            feedbackElement.className = 'feedback';
 
             // take the text box contents and compare to the answer
             var solution = parentNode.querySelector('textarea').value;
@@ -122,6 +123,7 @@ window.addEventListener("load", e => {
                 parentNode.insertBefore(feedbackElement, targetNode.nextSibling);
             }
             feedbackElement.id = feedbackElementId;
+            feedbackElement.className = 'feedback';
 
             // take the text box contents and compare to the answer
             var solution = parentNode.querySelector('textarea').value;
