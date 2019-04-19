cdd98721b9c0c24f4ea8aafc615fff8367db09c2
srogersact <srogersact@gmail.com>
2019-04-18 20:42:49 -0700

    BOWD Conventions 5
    Changed titles, also, to make tabs easier to recognize.

---
+++ tutorials-v4/BOWD_conventions_v5/problems.js
@@ -0,0 +1,75 @@
+var analysis_problems = [
+
+{
+analyzedSentence: 'the littmus lozenge tasted a little melancholy',
+composedSentence: 'The Littmus Lozenge tasted a little melancholy.',
+}, 
+{
+analyzedSentence: 'the preacher said that the littmus lozenge tasted a little melancholy ',
+composedSentence: 'The preacher said that the Littmus Lozenge tasted a little melancholy. ',
+}, 
+{
+analyzedSentence: 'the preacher said the littmus lozenge tastes a little melancholy ',
+composedSentence: 'The preacher said, "The Littmus Lozenge tastes a little melancholy." ',
+}, 
+{
+analyzedSentence: 'we shouldnt make other peoples tragedies the subject of idle conversation',
+composedSentence: 'We shouldn\'t make other people\'s tragedies the subject of idle conversation.',
+}, 
+{
+analyzedSentence: 'we shouldnt make other peoples tragedies the subject of idle conversation the preacher said',
+composedSentence: '"We shouldn\'t make other people\'s tragedies the subject of idle conversation," the preacher said.',
+}, 
+{
+analyzedSentence: 'there just myths she said',
+composedSentence: '"They\'re just myths," she said.',
+}, 
+{
+analyzedSentence: 'i was done talking i just stood their staring up at the constellations and planets',
+composedSentence: 'I was done talking. I just stood there staring up at the constellations and planets.',
+}, 
+{
+analyzedSentence: 'when i was done talking i just stood their staring up at the constellations and planets',
+composedSentence: 'When I was done talking, I just stood there staring up at the constellations and planets.',
+},
+
+
+];
+
+
+var composition_problems = [
+
+{
+analyzedSentence: 'the littmus lozenge tasted a little melancholy',
+composedSentence: 'The Littmus Lozenge tasted a little melancholy.',
+}, 
+{
+analyzedSentence: 'the preacher said that the littmus lozenge tasted a little melancholy ',
+composedSentence: 'The preacher said that the Littmus Lozenge tasted a little melancholy. ',
+}, 
+{
+analyzedSentence: 'the preacher said the littmus lozenge tastes a little melancholy ',
+composedSentence: 'The preacher said, "The Littmus Lozenge tastes a little melancholy." ',
+}, 
+{
+analyzedSentence: 'we shouldnt make other peoples tragedies the subject of idle conversation',
+composedSentence: 'We shouldn\'t make other people\'s tragedies the subject of idle conversation.',
+}, 
+{
+analyzedSentence: 'we shouldnt make other peoples tragedies the subject of idle conversation the preacher said',
+composedSentence: '"We shouldn\'t make other people\'s tragedies the subject of idle conversation," the preacher said.',
+}, 
+{
+analyzedSentence: 'there just myths she said',
+composedSentence: '"They\'re just myths," she said.',
+}, 
+{
+analyzedSentence: 'i was done talking i just stood their staring up at the constellations and planets',
+composedSentence: 'I was done talking. I just stood there staring up at the constellations and planets.',
+}, 
+{
+analyzedSentence: 'when i was done talking i just stood their staring up at the constellations and planets',
+composedSentence: 'When I was done talking, I just stood there staring up at the constellations and planets.',
+},
+
+];
