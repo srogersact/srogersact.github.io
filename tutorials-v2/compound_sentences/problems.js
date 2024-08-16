be3a80eaa570c004832ffb9ca23f531cf71b390c
srogersact <srogersact@gmail.com>
2024-08-16 11:46:25 -0700

    Update problems.js
    Change in punctuation for test

--- tutorials-v2/compound_sentences/problems.js
+++ tutorials-v2/compound_sentences/problems.js
@@ -16,8 +16,8 @@ var analysis_problems = [
         composedSentence: 'Many rivers flow from north to south, but the Nile River flows from south to north.',
     },
     {
-        analyzedSentence: 'To travel upstream on the Nile River in ships the Egyptians put up sails to catch the breeze that usually blew from north to south. However, to go north they put down the sails and rode the current that flowed downstream to the Mediterranean Sea.',
-        composedSentence: 'To travel upstream on the Nile River in ships the Egyptians put up sails to catch the breeze that usually blew from north to south, but to go north they put down the sails and rode the current that flowed downstream to the Mediterranean Sea.',
+        analyzedSentence: 'To travel upstream on the Nile River in ships, the Egyptians put up sails to catch the breeze that usually blew from north to south. However, to go north they put down the sails and rode the current that flowed downstream to the Mediterranean Sea.',
+        composedSentence: 'To travel upstream on the Nile River in ships, the Egyptians put up sails to catch the breeze that usually blew from north to south, but to go north they put down the sails and rode the current that flowed downstream to the Mediterranean Sea.',
     },
     {
         analyzedSentence: 'The many hills in Greece could sometimes be used for growing barley and were often used for growing olives and grapes. However, sometimes the hills were steep enough that they could only be used for grazing sheep and goats.',
